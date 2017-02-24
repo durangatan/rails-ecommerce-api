d07ebac43c59a3a31d4ea113e93c5362adfcdb67
Joe Duran <joeduran8@gmail.com>
2017-02-23 23:45:24 -0600

    adds item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  has_many :purchases
+  has_many :orders, through: :purchases
+  has_many :customers, through: :orders
+  has_many :categorizations
+  has_many :categories, through: :categorizations
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
+
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_many(:purchases) }
+  it { should have_many(:orders).through(:purchases)}
+  it { should have_many(:customers).through(:orders)}
+  it { should have_many(:categorizations)}
+  it { should have_many(:categories).through(:categorizations)}
 end
