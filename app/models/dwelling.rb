1fb416ea941f79bd685b3bc5cb1a1e9594150431
Joe Duran <joeduran8@gmail.com>
2017-02-23 23:45:15 -0600

    adds dwelling model

--- app/models/dwelling.rb
+++ app/models/dwelling.rb
@@ -1,2 +1,4 @@
 class Dwelling < ApplicationRecord
+  belongs_to :customer
+  belongs_to :postal_address, foreign_key: "address_id"
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_many(:dwellings).dependent(:destroy)}
+  it { should have_many(:postal_addresses).through(:dwellings)}
+  it { should have_many(:orders)}
+  it { should have_many(:purchases).through(:orders)}
+  it { should have_many(:purchased_items).through(:purchases).source(:item)}
+
 end
