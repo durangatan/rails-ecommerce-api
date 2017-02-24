ecb7f5356d29af20879bbe7214c19da1d4185c59
Joe Duran <joeduran8@gmail.com>
2017-02-23 23:45:35 -0600

    adds order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :purchases, dependent: :destroy
+
+  def item_total
+    purchases.map(&:total).reduce(:+)
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @order = create(:order_with_purchases)
+  end
+  it {should belong_to(:customer)}
+  it {should have_many(:purchases).dependent(:destroy)}
+
+  it "knows its item total" do
+    expect(@order.item_total).to eq 12
+  end
+
 end
