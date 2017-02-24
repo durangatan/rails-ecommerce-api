2137385e6b3be90abe68bf62363ed5fd9768da28
Joe Duran <joeduran8@gmail.com>
2017-02-23 23:45:49 -0600

    adds order and item factories

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :item do
-    
+    price 4
+    unique false
+
   end
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :order do
-    
+    customer
+    factory :order_with_purchases do
+      after(:create) do |order|
+        create(:purchase, order:order)
+      end
+    end
   end
 end
