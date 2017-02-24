c13f7c65e7ffced3ab9f173eebe490ec412bce75
Joe Duran <joeduran8@gmail.com>
2017-02-23 23:45:03 -0600

    adds customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,12 @@
 class Customer < ApplicationRecord
-    has_many :dwellings
-    has_many :postal_addresses, through: :dwellings
-    has_many :purchases
-    has_many :orders, through: :purchases
-    has_many :purchased_items, through: :purchases, class_name: item
+  has_many :dwellings, dependent: :destroy
+  has_many :postal_addresses, through: :dwellings
+  has_many :orders
+  has_many :purchases, through: :orders
+  has_many :purchased_items, through: :purchases, source: :item
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 end
