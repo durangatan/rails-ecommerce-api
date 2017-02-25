46402f52d6e8f8bed0d90336bf03caa9d95930ce
Joe Duran <joeduran8@gmail.com>
2017-02-25 13:17:53 -0600

    temporarily enable all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  # devise_for :customers
-  # resources :items do
-  #   resources :categorizations
-  # end
-  # resources :categories
-  # resources :orders do
-  #   resources :purchases
-  # end
-  # resources :customers do
-  #   resources :postal_addresses
-  # end
+  devise_for :customers
+  resources :items do
+    resources :categorizations
+  end
+  resources :categories
+  resources :orders do
+    resources :purchases
+  end
+  resources :customers do
+    resources :postal_addresses
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
