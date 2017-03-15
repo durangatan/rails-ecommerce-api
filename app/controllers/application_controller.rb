c0b34b38de532fd0ac0a7c551ba87f82f4681f5f
Joe Duran <joeduran8@gmail.com>
2017-03-14 23:27:52 -0500

    add authenticate_customer! method to base controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  def authenticate_customer!
+    token, options = ActionController::HttpAuthentication::Token.token_and_options(request)
+
+    customer_email = options.blank?? nil : options[:email]
+    customer = customer_email && Customer.find_by(email: customer_email)
+    customer && ActiveSupport::SecurityUtils.secure_compare(customer.auth_token, token)
+  end
 end
