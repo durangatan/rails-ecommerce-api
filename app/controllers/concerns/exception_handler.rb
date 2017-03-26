0aeb1d45b0ba4d6d699e2817d26be1a5f931e10a
Joe Duran <joeduran8@gmail.com>
2017-03-26 15:07:40 -0500

    use proper syntax in exception_handler

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,14 +3,14 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({message:e.message}, :not_found)
+      json_response({ message: e.message }, :not_found)
     end
 
     rescue_from ActiveRecord::StatementInvalid do |e|
-      json_response({message:e.message}, :unprocessable_entity)
+      json_response({ message: e.message }, :unprocessable_entity)
     end
     rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({message:e.message}, :unprocessable_entity)
+      json_response({ message: e.message }, :unprocessable_entity)
     end
   end
 end
