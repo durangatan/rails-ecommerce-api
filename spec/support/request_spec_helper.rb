b2361ee9dd497bc9654e54a678684a10e0c1b301
Joe Duran <joeduran8@gmail.com>
2017-03-26 15:11:22 -0500

    adds helper methods for authentication tests

--- spec/support/request_spec_helper.rb
+++ spec/support/request_spec_helper.rb
@@ -3,4 +3,30 @@ module RequestSpecHelper
   def json
     JSON.parse(response.body)
   end
+
+  def get_with_token(path, params={}, headers={})
+    headers.merge!('Authorization: Token' => @token)
+    get path, params: params, headers: headers
+  end
+
+  def post_with_token(path, params={}, headers={})
+    headers.merge!('Authorization: Token' => @token)
+    post path, params: params, headers:headers
+  end
+
+  def put_with_token(path, params={}, headers={})
+    headers.merge!('Authorization: Token' => @token)
+    put path, params: params, headers:headers
+  end
+
+  def delete_with_token(path, params={}, headers={})
+    headers.merge!('Authorization: Token' => @token)
+    delete path, params: params, headers:headers
+  end
+
+  def login_and_get_token(credentials={email:'admin@mail.com', password:'secret'})
+    post '/login', params: credentials
+    @token = json['token']
+  end
+
 end
