f2eea6b1ce2c5542ffe31faf1ebf97b430e75307
Joe Duran <joeduran8@gmail.com>
2017-03-26 15:09:19 -0500

    updates test database seed to use realistic data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318030120) do
+ActiveRecord::Schema.define(version: 20170319045024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170318030120) do
     t.datetime "updated_at",                             null: false
     t.string   "password_digest"
     t.string   "auth_token"
+    t.boolean  "admin",                  default: false, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true, using: :btree
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,30 @@ PostalAddress.delete_all
 Purchase.delete_all
 
 
-categoryNew = Category.create(name:"New Instruments",thumbnail: "seriesiisop.jpg");
-categoryUsed = Category.create(name:"Used Instruments",thumbnail: "markviten.png");
+categoryNew = Category.create(name:'New Instruments',thumbnail: 'seriesiisop.jpg');
+categoryUsed = Category.create(name:'Used Instruments',thumbnail: 'markviten.png');
 
-usedAltos = Category.create(name:"Used Alto Saxophones", parent_id:categoryUsed.id, thumbnail: "cigas.jpg")
-newAltos = Category.create(name:"New Alto Saxophones", parent_id:categoryNew.id, thumbnail: "seriesiiias.jpg")
-usedClarinets = Category.create(name:"Used Clarinets", parent_id:categoryUsed.id, thumbnail: "buffet-r13-clarinet.png")
-usedTenors = Category.create(name:"Used Tenor Saxophones", parent_id:categoryUsed.id,thumbnail:"markviten.png")
-usedSopranos = Category.create(name:"Used Soprano Saxophones", parent_id:categoryUsed.id, thumbnail:"seriesiisop.jpg")
+usedAltos = Category.create(name:'Used Alto Saxophones', parent_id:categoryUsed.id, thumbnail: 'cigas.jpg')
+newAltos = Category.create(name:'New Alto Saxophones', parent_id:categoryNew.id, thumbnail: 'seriesiiias.jpg')
+usedClarinets = Category.create(name:'Used Clarinets', parent_id:categoryUsed.id, thumbnail: 'buffet-r13-clarinet.png')
+usedTenors = Category.create(name:'Used Tenor Saxophones', parent_id:categoryUsed.id,thumbnail:'markviten.png')
+usedSopranos = Category.create(name:'Used Soprano Saxophones', parent_id:categoryUsed.id, thumbnail:'seriesiisop.jpg')
 customer = Customer.create!(
-  email: "test@mail.com",
-  password: "secret",
+  email: 'test@mail.com',
+  password: 'secret',
   braintree_customer_id: nil,
   name: Faker::Name.name,
   phone: Faker::PhoneNumber.phone_number,
   verified_email: false
+)
+Customer.create!(
+  email: 'admin@mail.com',
+  password: 'secret',
+  braintree_customer_id: nil,
+  name: Faker::Name.name,
+  phone: Faker::PhoneNumber.phone_number,
+  verified_email: false,
+  admin: true
 )
 address = PostalAddress.create(
   street: Faker::Address.street_address,
@@ -43,28 +52,28 @@ Dwelling.create(
 
 
 itemArray =[{
-              name:"Buffet R13 Clarinet",
-              thumbnail:"buffet-r13-clarinet.png",
+              name:'Buffet R13 Clarinet',
+              thumbnail:'buffet-r13-clarinet.png',
               category:usedClarinets,
               featured:true,
             price:240000},{
-              name:"Selmer Cigar Cutter",
-              thumbnail:"cigas.jpg",
+              name:'Selmer Cigar Cutter',
+              thumbnail:'cigas.jpg',
               featured: false,
               category:usedAltos,
             price:500000},{
-              name:"Selmer Mark VI Tenor Saxophone",
-              thumbnail:"markviten.png",
+              name:'Selmer Mark VI Tenor Saxophone',
+              thumbnail:'markviten.png',
               featured: true,
               category:usedTenors,
             price:1000000},{
-              name:"Selmer Series III Alto Saxophone",
-              thumbnail:"seriesiiias.jpg",
+              name:'Selmer Series III Alto Saxophone',
+              thumbnail:'seriesiiias.jpg',
               featured: false,
               category:newAltos,
             price:500000},{
-              name:"Selmer Series II Soprano Saxophone",
-              thumbnail:"seriesiisop.jpg",
+              name:'Selmer Series II Soprano Saxophone',
+              thumbnail:'seriesiisop.jpg',
               featured: false,
               price:400000,
 category:usedSopranos}]
@@ -72,7 +81,7 @@ category:usedSopranos}]
 itemArray.each do |item|
 
   unique =[true,false].sample
-  availability = ["in-stock", "back-order", "sold-out"].sample
+  availability = ['in-stock', 'back-order', 'sold-out'].sample
   new_item = Item.create(
     name: item[:name],
     price: item[:price],
