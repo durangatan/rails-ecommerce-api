31c3d9f50d6aa8c90becd7171d05d227ce4290c3
Joe Duran <joeduran8@gmail.com>
2017-03-26 15:07:06 -0500

    add authentication scheme in Orders Controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,8 @@
 class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :update, :destroy]
-  before_action :authenticate_customer!
-
+  before_action :authenticate_customer!, only: [:show, :update, :index, :destroy]
+  before_action :admin_only!, only: [:destroy, :index]
+  before_action :own_orders_only!, only: [:show, :update]
   def index
     @orders = Order.all
     json_response(@orders)
@@ -34,5 +35,9 @@ class OrdersController < ApplicationController
 
   def set_order
     @order = Order.find(params[:id])
+  end
+
+  def own_orders_only!
+    return json_response({} ,:unauthorized) unless (order_params[:customer_id] == @customer.id) || @customer.admin
   end
 end
