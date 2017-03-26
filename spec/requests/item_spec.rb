192b27ccaf57ed06b52110e8c703ae0c810ac60e
Joe Duran <joeduran8@gmail.com>
2017-03-26 15:09:48 -0500

    updates Item model tests to use authentication

--- spec/requests/item_spec.rb
+++ spec/requests/item_spec.rb
@@ -1,40 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Items API', type: :request do
+  let!(:admin) { create(:admin) }
+  before { login_and_get_token }
+  let!(:items) { create_list(:item, 10) }
+  let(:item_id) { Item.first.id }
 
-  let!(:items){ create_list(:item, 10)}
-  let(:item_id){Item.first.id}
+  describe 'GET /items' do
+    before { get '/items' }
 
-  describe "GET /items" do
-    before { get "/items"}
-
-    it "returns an array of items" do
+    it 'returns an array of items' do
       expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
 
-    it "returns status code 200" do
+    it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /items/:id" do
-    before { get "/items/#{item_id}"}
+  describe 'GET /items/:id' do
+    before { get "/items/#{item_id}" }
 
-    context "when the item exists" do
-      it "returns the item" do
+    context 'when the item exists' do
+      it 'returns the item' do
         expect(json).not_to be_empty
-        expect(json["item"]["id"]).to eq(item_id)
+        expect(json['item']['id']).to eq(item_id)
       end
 
-      it "returns status code 200" do
+      it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
     end
 
-    context "when the record does not exist" do
-      let(:item_id){100}
-      it "returns status code 404" do
+    context 'when the record does not exist' do
+      let(:item_id) { 100 }
+      it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
 
@@ -44,11 +45,11 @@ RSpec.describe 'Items API', type: :request do
     end
   end
 
-  describe "POST /items" do
-    let(:valid_attributes){{ name:"Selmer Mark VI", price:"3000", availability: "in stock", unique:"true"}}
+  describe 'POST /items' do
+    let(:valid_attributes) { { name:'Selmer Mark VI', price: '3000', availability: 'in stock', unique: 'true' } }
 
     context 'when the request is valid' do
-      before { post "/items", params: valid_attributes }
+      before { post_with_token '/items', valid_attributes }
       it 'creates a item' do
         expect(json['name']).to eq('Selmer Mark VI')
       end
@@ -59,20 +60,20 @@ RSpec.describe 'Items API', type: :request do
     end
 
     context 'when the request is invalid' do
-      before { post "/items", params: {name: "jazz"}}
+      before { post_with_token '/items', { name: 'jazz' } }
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
       end
-      it "returns a validation failure message" do
+      it 'returns a validation failure message' do
         expect(response.body).to match(/PG::NotNullViolation:/)
       end
     end
   end
 
   describe 'PUT /items/:id' do
-    let(:valid_attributes){{ name: "King Super 20"}}
+    let(:valid_attributes) { { name: 'King Super 20' } }
     context 'when the record exists' do
-      before {put "/items/#{item_id}", params: valid_attributes}
+      before { put_with_token "/items/#{item_id}", valid_attributes }
 
       it 'updates the record' do
         expect(response.body).to be_empty
@@ -85,8 +86,8 @@ RSpec.describe 'Items API', type: :request do
   end
 
   describe 'DELETE /items/:id' do
-    before { delete "/items/#{item_id}"}
-    it "returns status code 204" do
+    before { delete_with_token "/items/#{item_id}" }
+    it 'returns status code 204' do
       expect(response).to have_http_status 204
     end
   end
